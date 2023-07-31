beb1054b068295d6f746d2c88713025bd8eec0c4
Emmanuel Ramos <desarrollador03.ti@fcr.com.co>
2023-07-31 13:22:15 -0500

    fixed

--- src/models/devialarm.js
+++ src/models/devialarm.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(model) {
-      this.belongsTo(model.keywords, {as: 'keywords', foreignKey: 'keywnuid'});
+      this.belongsTo(model.keywords, {as: 'keywords', foreignKey: 'keywdeal'});
     }
   }
   devialar.init({
