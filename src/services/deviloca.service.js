ac9084897b04427fd5513d7d889e57f14af8c433
Emmanuel Ramos <desarrollador03.ti@fcr.com.co>
2023-07-31 13:09:23 -0500

    search

--- src/services/deviloca.service.js
+++ src/services/deviloca.service.js
@@ -62,10 +62,10 @@ const createLocation = async (payload) => {
     const parseLonSearch = parseFloat(lastRecordPark[0].delolong.toString().replace(/\./g, ''));
 
     const validate = calculateDifference(parseLat, parseLatSearch, parseLon, parseLonSearch, 100);
-    if (payload.deloacc == 1 && (!validateEvent || validateEvent.devialar.keywcodi == 'end_ralenti')) {
+    if (payload.deloacc == 1 && (!validateEvent || validateEvent.keywcodi == 'end_ralenti')) {
       payloadAlarmType = 22;
       createAlarm = true;
-    } else if (payload.deloacc == 0 && validateEvent && validateEvent.devialar.keywcodi == 'on_ralenti') {
+    } else if (payload.deloacc == 0 && validateEvent && validateEvent.keywcodi == 'on_ralenti') {
       payloadAlarmType = 23;
       createAlarm = true;
     }
@@ -90,7 +90,7 @@ const createLocation = async (payload) => {
       );
     }
   } else {
-    if (validateEvent && validateEvent.devialar.keywcodi == 'on_ralenti') {
+    if (validateEvent && validateEvent.keywcodi == 'on_ralenti') {
       payloadAlarmType = 23;
       createAlarm = true;
     }
