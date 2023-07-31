9f0f9295fd76efbbed02181e7176b8447c531057
Emmanuel Ramos <desarrollador03.ti@fcr.com.co>
2023-07-31 08:50:13 -0500

    other way ralenti

--- src/services/deviloca.service.js
+++ src/services/deviloca.service.js
@@ -66,10 +66,10 @@ const createLocation = async (payload) => {
 
     const validate = calculateDifference(parseLat, parseLatSearch, parseLon, parseLonSearch, 100);
 
-    if (payload.deloacc == 1 && (!validateEvent || validateEvent.keywfunc == 'end_ralenti')) {
+    if (payload.deloacc == 0 && (!validateEvent || validateEvent.keywfunc == 'end_ralenti')) {
       payloadAlarmType = 22;
       createAlarm = true;
-    } else if (payload.deloacc == 0 && validateEvent && validateEvent.keywfunc == 'on_ralenti') {
+    } else if (payload.deloacc == 1 && validateEvent && validateEvent.keywfunc == 'on_ralenti') {
       payloadAlarmType = 23;
       createAlarm = true;
     }
