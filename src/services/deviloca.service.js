23a127b64a87c3d4bd4ea79b55d6b11cb420698b
Emmanuel Ramos <desarrollador03.ti@fcr.com.co>
2023-08-08 15:09:16 -0500

    fixed comparation

--- src/services/deviloca.service.js
+++ src/services/deviloca.service.js
@@ -92,9 +92,9 @@ const createLocation = async (payload) => {
       console.log(validateEventPark);
       console.log("---------------------------");
     }
-    if (deloacc === 0 && lastRecordPark[0].deloacc === '0' && (!validateEventPark || validateEventPark.keywords.keywcodi === 'end_parking')) {
+    if (deloacc === '0' && lastRecordPark[0].deloacc === '0' && (!validateEventPark || validateEventPark.keywords.keywcodi === 'end_parking')) {
       await createAlarmIfValid(true, 20);
-    } else if (deloacc === 1 && validateEventPark && validateEventPark.keywords.keywcodi === 'on_parking') {
+    } else if (deloacc === '1' && validateEventPark && validateEventPark.keywords.keywcodi === 'on_parking') {
       await createAlarmIfValid(true, 21);
     }
 
